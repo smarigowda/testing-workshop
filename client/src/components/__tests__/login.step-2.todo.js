2c86f0f41bb0bb13435bc699b69192eab2f06941
Santosh Marigowda <santosharakere@gmail.com>
2018-06-10 07:05:39 +0100

    login step 2 complete

--- client/src/components/__tests__/login.step-2.todo.js
+++ client/src/components/__tests__/login.step-2.todo.js
@@ -2,7 +2,7 @@
 import React from 'react'
 // import ReactDOM from 'react-dom'
 // you'll need these:
-// import {generate} from 'til-client-test-utils'
+import {generate} from 'til-client-test-utils'
 import {render, Simulate} from 'react-testing-library'
 // note that til-client-test-utils is found in `client/test/til-client-test-utils`
 import Login from '../login'
@@ -10,26 +10,30 @@ import Login from '../login'
 test('calls onSubmit with the username and password when submitted', () => {
   // Arrange
   // use generate.loginForm() here
-  const fakeUser = {username: 'chucknorris', password: '(╯°□°）╯︵ ┻━┻'}
+  // const fakeUser = {username: 'chucknorris', password: '(╯°□°）╯︵ ┻━┻'}
+  const fakeUser = generate.loginForm()
+
   const handleSubmit = jest.fn()
   // use: render(<Login onSubmit={handleSubmit} />)
   // It'll give you back an object with
   // `getByLabelText` and `getByText` functions
   // so you don't need a div anymore!
-  const { container, getByLabelText, getByText } = render(<Login onSubmit={handleSubmit} />);
+  const {container, getByLabelText, getByText} = render(
+    <Login onSubmit={handleSubmit} />,
+  )
 
   // const inputs = div.querySelectorAll('input')
-  const usernameNode = getByLabelText('Username');
-  const passwordNode = getByLabelText('Password');
-  const formNode = container.querySelector('form');
-  const submitButtonNode = getByText('Submit');
+  const usernameNode = getByLabelText('Username')
+  const passwordNode = getByLabelText('Password')
+  const formNode = container.querySelector('form')
+  const submitButtonNode = getByText('Submit')
 
   usernameNode.value = fakeUser.username
   passwordNode.value = fakeUser.password
 
   // Act
   // Use Simulate.submit(formNode) instead of these two lines
-  Simulate.submit(formNode);
+  Simulate.submit(formNode)
   // Assert
   // no change necessary here
   expect(handleSubmit).toHaveBeenCalledTimes(1)
@@ -46,8 +50,8 @@ test('calls onSubmit with the username and password when submitted', () => {
 /*
 http://ws.kcd.im/?ws=Testing&e=login.step-2%20(react-testing-library)&em=santosharakere@gmail.com
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
